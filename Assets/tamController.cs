1804a8cb25cded69cd274fab6a131c0404601c12
SleepyJune <hshsiao63@gmail.com>
2020-09-05 13:08:24 -0700

    Initial commit

---
+++ Assets/tamController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tamController : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip tapSound;
+
+    public float ShakeDetectionThreshold;
+    public float MinShakeInterval;
+
+    private float sqrShakeDetectionThreshold;
+    private float timeSinceLastShake = 0;
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+    
+    void Start()
+    {
+        sqrShakeDetectionThreshold = Mathf.Pow(ShakeDetectionThreshold, 2);
+    }
+    void Update()
+    {
+        if (Input.acceleration.sqrMagnitude >= sqrShakeDetectionThreshold
+                   && Time.unscaledTime >= timeSinceLastShake + MinShakeInterval)
+        {            
+            timeSinceLastShake = Time.unscaledTime;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        audioSource.PlayOneShot(tapSound);
+    }
+}
